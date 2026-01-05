77d37b312175a565d30645e03a38585fa41216ec
YoussefBA10 <youssef.benarous@esprit.tn>
2026-01-05 16:46:31 +0100

    Dockerfile update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { initFaro } from './services/faro';
+//import { initFaro } from './services/faro';
 
-initFaro();
+//initFaro();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
